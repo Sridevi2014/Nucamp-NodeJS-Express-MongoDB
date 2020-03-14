2dbd6fab42558a457d878f189add55d91283eb5a
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-03-14 11:45:30 -0500

    comments

--- node-express/routes/campsiteRouter.js
+++ node-express/routes/campsiteRouter.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser'); //jason body parser
+const bodyParser = require('body-parser'); //json body parser
 
 const campsiteRouter = express.Router();
 
