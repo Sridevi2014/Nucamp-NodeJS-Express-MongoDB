b86e768c320acf9af956ca58dd138728d76647a2
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-03-14 11:43:52 -0500

    comments

--- node-express/server.js
+++ node-express/server.js
@@ -6,23 +6,21 @@ const campsiteRouter = require('./routes/campsiteRouter'); //importe
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
 
-
-const hostname = 'localhost'; //we created server instence
+const hostname = 'localhost'; //we created server instance
 const port = 3000;
 
 const app = express();// calling express function / using middlewar
 app.use(morgan('dev'));
 app.use(bodyParser.json());
 
-//if user requests campsites url, campsiteRouter going display all campsite details
+//if user requests campsites url, campsiteRouter going to display all campsite details
 app.use('/campsites', campsiteRouter);
-//if user requests promotions url, promotionsRouter going promotions all promotion details
+//if user requests promotions url, promotionsRouter going to promotions all promotion details
 app.use('/promotions', promotionRouter);
-//if user requests partners url, partnerRouter going display all partner details
+//if user requests partners url, partnerRouter going to display all partner details
 app.use('/partners', partnerRouter);
 
-
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); //grab static files from public dir
 
 app.use((req, res) => {
     res.statusCode = 200;
